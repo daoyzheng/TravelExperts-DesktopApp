2466ada926c08aed390c70188f1a45cef8b1fd69
Tim Leslie <tdleslie1@gmail.com>
2019-03-25 16:19:45 -0600

    Addition of simple class definitions for Products, Suppliers, and PackagesProductsSuppliers.  March 25, 2019. Tim Leslie.

---
+++ Workshop4/ClassEntites/Products.cs
@@ -0,0 +1,25 @@
+﻿/********************************************************************************
+* 
+* Author: Tim Leslie
+* Date: March 25, 2019.
+* Course: CPRG 217 Rapid OOSD Threaded Project
+* Assignment: Workshop 4
+* Purpose: This is a Product class definition and forms part of the CPRG 214
+* Threaded Project Workshop 4.
+*
+*********************************************************************************/using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassEntites
+{
+    // Product class definition mirroring the Travel Experts product table.
+    public class Products
+    {
+        public int ProductId { get; set; }
+
+        public string ProdName { get; set; }
+    }
+}
---
+++ Workshop4/ClassEntites/Suppliers.cs
@@ -0,0 +1,26 @@
+﻿/********************************************************************************
+* 
+* Author: Tim Leslie
+* Date: March 25, 2019.
+* Course: CPRG 217 Rapid OOSD Threaded Project
+* Assignment: Workshop 4
+* Purpose: This is a Suppliers class definition and forms part of the CPRG 214
+* Threaded Project Workshop 4.
+*
+*********************************************************************************/using System;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassEntites
+{
+    // Suppliers class definition which mirrors the Travel Experts Suppliers table.
+    public class Suppliers
+    {
+        public int SupplierId { get; set; }
+
+        public string ProdName { get; set; } // string is reference type and thus nullable
+    }
+}
